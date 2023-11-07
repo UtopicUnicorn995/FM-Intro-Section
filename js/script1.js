ceca82307b9246e66edc708399a2a73b03843772
utopicUnicorn <christianbarrios995@gmail.com>
2023-11-07 19:16:05 +0800

    fm-intro: added media queries

--- js/script1.js
+++ js/script1.js
@@ -4,4 +4,10 @@ $(function(){
         $('.header__nav-list .header__nav-item.nav-features span').toggleClass('down')
         $('.nav-features').toggleClass('hidden')
     })
+
+    $('.nav-company').click(function(){
+        $('.header__nav-list .header__nav-item.nav-company span').toggleClass('up')
+        $('.header__nav-list .header__nav-item.nav-company span').toggleClass('down')
+        $('.nav-company').toggleClass('hidden')
+    })
 })
